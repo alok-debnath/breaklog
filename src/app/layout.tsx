72934b64636d40984341aa4cae54ab4175c3cec8
Alok <alokdebnath.in@gmail.com>
2023-09-09 13:12:18 +0530

    added vercel analitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
+        <Analytics />
       </body>
     </html>
   )
