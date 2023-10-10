b3102c5c3ce90eb3d38f7d85c5cf3ecc390ce973
Alok Debnath <67475527+alok-debnath@users.noreply.github.com>
2023-10-10 18:52:35 +0000

    profile

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -39,12 +39,57 @@ const ProfilePage = () => {
         <div className='hero min-h-screen bg-base-200'>
           <div className='hero-content text-center'>
             <div className='max-w-md'>
-              <h1 className='text-5xl font-bold'>Hello there</h1>
-              <p className='py-6'>
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
-                exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
-              </p>
-              <button className='btn btn-primary'>Get Started</button>
+              <div className='card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100'>
+                <form className='card-body grid gap-y-3 w-full max-w-xl'>
+                  <h3 className='text-2xl font-bold text-left'>Update your data</h3>
+                  <div className='form-control'>
+                    <label
+                      className='label'
+                      htmlFor='username'>
+                      <span className='label-text'>Username</span>
+                    </label>
+                    <input
+                      type='text'
+                      id='username'
+                      placeholder='Type here'
+                      className='input input-bordered w-full max-w-md'
+                    />
+                  </div>
+                  <div className='form-control'>
+                    <label
+                      className='label'
+                      htmlFor='email'>
+                      <span className='label-text'>Email</span>
+                    </label>
+                    <input
+                      type='email'
+                      id='email'
+                      placeholder='Type here'
+                      className='input input-bordered w-full max-w-xs'
+                    />
+                  </div>
+                  <div className='form-control'>
+                    <label
+                      className='label'
+                      htmlFor='name'>
+                      <span className='label-text'>Name (not required)</span>
+                    </label>
+                    <input
+                      type='text'
+                      id='name'
+                      placeholder='Type here'
+                      className='input input-bordered w-full max-w-xs'
+                    />
+                  </div>
+                  <div className='form-control mt-6'>
+                    <button
+                      type='submit'
+                      className='btn btn-primary normal-case'>
+                      Update
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
