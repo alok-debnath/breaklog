9316d0142fb4d15a37d3fa1677d197f78c0987a1
Alok Debnath <alokdebnath.in@gmail.com>
2023-11-06 19:15:48 +0530

    UI updates

--- src/app/dashboard/history/page.tsx
+++ src/app/dashboard/history/page.tsx
@@ -181,22 +181,24 @@ const HistoryPage = () => {
                         <thead>
                           <tr>
                             <th>
-                              <span
-                                className='flex items-center tooltip tooltip-right cursor-pointer'
-                                data-tip='Click any of the dates'>
-                                <svg
-                                  xmlns='http://www.w3.org/2000/svg'
-                                  fill='none'
-                                  viewBox='0 0 24 24'
-                                  strokeWidth={1.5}
-                                  stroke='currentColor'
-                                  className='w-6 h-6 me-1 text-warning'>
-                                  <path
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                    d='M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z'
-                                  />
-                                </svg>
+                              <span className='flex items-center'>
+                                <span
+                                  className='tooltip tooltip-right cursor-pointer'
+                                  data-tip='Click on any of the dates'>
+                                  <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    fill='none'
+                                    viewBox='0 0 24 24'
+                                    strokeWidth={1.5}
+                                    stroke='currentColor'
+                                    className='w-6 h-6 me-1 text-warning'>
+                                    <path
+                                      strokeLinecap='round'
+                                      strokeLinejoin='round'
+                                      d='M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z'
+                                    />
+                                  </svg>
+                                </span>
                                 Date
                               </span>
                             </th>
