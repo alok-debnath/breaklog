64fba7c6762d08f789f929780f41104505318077
Alok Debnath <alokdebnath.in@gmail.com>
2024-02-02 18:08:59 +0000

    test fix for logout

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -6,10 +6,15 @@ export async function GET() {
       message: 'Logged successfully',
       success: true,
     });
+    
+    // Set cache control header to 'no-store'
+    response.headers.set('Cache-Control', 'no-store');
+
     response.cookies.set('token', '', {
       httpOnly: true,
       expires: new Date(0),
     });
+
     return response;
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
