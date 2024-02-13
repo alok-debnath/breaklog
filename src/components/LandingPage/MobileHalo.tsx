832f155e9b9b28d13820a1f903df6de42474faec
Alok Debnath <alokdebnath.in@gmail.com>
2024-02-13 18:55:28 +0000

    minor change

--- src/components/LandingPage/MobileHalo.tsx
+++ src/components/LandingPage/MobileHalo.tsx
@@ -1,6 +1,6 @@
 const MobileHalo = () => {
   return (
-    <div className='mobile-halo-div hidden md:block w-max'>
+    <div className='mobile-halo-div hidden md:block max-w-max'>
       <div className='mobile-halo' />
     </div>
   );
