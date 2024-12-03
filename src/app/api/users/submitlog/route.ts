f65c5d883d37f703dffcd3c172d81f17424641a3
Alok.k <alok.k@vandalayglobal.com>
2024-12-03 22:54:52 +0530

    test change to add HalfDay logs

--- src/app/api/users/submitlog/route.ts
+++ src/app/api/users/submitlog/route.ts
@@ -75,6 +75,7 @@ export async function POST(request: NextRequest) {
           },
           // updatedAt: datetime,
           log_status: logToBeSaved,
+          // isHalfDay: false,
         },
       });
     }
