79df1f376a884f1bf270aeb17f3283a532d6b50f
Alok Debnath <67475527+alok-debnath@users.noreply.github.com>
2025-03-19 16:12:36 +0000

    .

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,10 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    //  "next/typescript"
+  ),
 ];
 
 export default eslintConfig;
