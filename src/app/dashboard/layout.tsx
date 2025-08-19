2308dad713a45db010ca78cd71704c66037a87f9
Alok Debnath <alokdebnath.in@gmail.com>
2025-08-19 10:05:21 +0000

    feat: add force-dynamic export for dsahboard layout component

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { Suspense } from 'react';
 import ConfirmationModal from '@/components/Layouts/Modals/ConfirmationModal';
 import Navbar from '@/components/Layouts/Navbar';
